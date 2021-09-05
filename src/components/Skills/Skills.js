b6d8bc3e52431b87b7a2d6ce276e890f25587def
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2021-09-05 14:17:24 +0500

    First commit

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,107 @@
+/* eslint-disable no-lone-blocks */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Skills.css';
+
+const Skills = () => {
+    return (
+        <Container fluid>
+            <Container>
+                <div class="SkillsProfile">
+                    <h1>Skills Overview</h1>
+                </div>
+                <Row>
+                    <div class="col-sm">
+                        <div className="HTMLBox">
+                            <div className="circle">
+                            </div>
+                            <div className="FrontEndLists">
+                                <h4>Front End Development</h4>
+                                <ul>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>Javascript</li>
+                                    <li>Bootstrap</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div className="HTMLBox">
+                            <div className="circle">
+                            </div>
+                            <div className="FrontEndLists">
+                                <h4>Front End Development</h4>
+                                <ul>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>Javascript</li>
+                                    <li>Bootstrap</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div className="HTMLBox">
+                            <div className="circle">
+                            </div>
+                            <div className="FrontEndLists">
+                                <h4>Front End Development</h4>
+                                <ul>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>Javascript</li>
+                                    <li>Bootstrap</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div className="HTMLBox">
+                            <div className="circle">
+                            </div>
+                            <div className="FrontEndLists">
+                                <h4>Front End Development</h4>
+                                <ul>
+                                    <li>HTML</li>
+                                    <li>CSS</li>
+                                    <li>Javascript</li>
+                                    <li>Bootstrap</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </Row>
+            </Container>
+        </Container>
+    )
+}
+
+export default Skills;
+
+
+{/* <div className="HTMLBox">
+                            <div className="circle">
+                                
+                            </div>
+                            <div className="FrontEndLists">
+                                <h4>Front End Development</h4>
+                                    <ul>
+                                        <li>HTML</li>
+                                        <li>CSS</li>
+                                        <li>Javascript</li>
+                                        <li>Bootstrap</li>
+                                    </ul>
+                                </div>
+                            </div>
+                    </div>
+                    <div className="col-12 col-lg-4 mb-5 mb-3 mb-lg-0">
+                        <div className="HTMLBox">
+                            <div className="circle"></div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-4 mb-5 mb-3 mb-lg-0">
+                        <div className="HTMLBox">
+                            <div className="circle"></div>
+                        </div> */}
